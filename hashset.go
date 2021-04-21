32b95e28394cba378efce601b218829aeb6ac83d
Ye Li <liye.e@bytedance.com>
2021-04-21 16:18:58 +0800

    fix linter

--- hashset.go
+++ hashset.go
@@ -1,28 +1,21 @@
 package hashset
 
-import ()
-
-type Int64Set struct {
-	set    map[int64]struct{}
-	length int64
-}
+type Int64Set map[int64]struct{}
 
 func NewInt64() *Int64Set {
-	result := Int64Set{make(map[int64]struct{}), 0}
-	return &result
+	return &Int64Set{}
 }
 
 func (s *Int64Set) Add(value int64) bool {
 	if s.Contains(value) {
 		return false
 	}
-	(*s).length++
-	(*s).set[value] = struct{}{}
+	(*s)[value] = struct{}{}
 	return true
 }
 
 func (s *Int64Set) Contains(value int64) bool {
-	if _, ok := (*s).set[value]; ok {
+	if _, ok := (*s)[value]; ok {
 		return true
 	}
 	return false
@@ -30,19 +23,18 @@ func (s *Int64Set) Contains(value int64) bool {
 
 func (s *Int64Set) Remove(value int64) bool {
 	if s.Contains(value) {
-		delete((*s).set, value)
-		(*s).length--
+		delete(*s, value)
 		return true
 	}
 	return false
 }
 
 func (s *Int64Set) Range(f func(value int64) bool) {
-	for k, _ := range (*s).set {
+	for k := range *s {
 		f(k)
 	}
 }
 
 func (s *Int64Set) Len() int {
-	return int(len((*s).set))
+	return int(len(*s))
 }
--- hashset_test.go
+++ hashset_test.go
@@ -10,53 +10,53 @@ func Example() {
 
 	for _, v := range []int{10, 12, 15} {
 		if l.Add(int64(v)) {
-			fmt.Println("skipset add", v)
+			fmt.Println("hashset add", v)
 		}
 	}
 
 	if l.Contains(10) {
-		fmt.Println("skipset contains 10")
+		fmt.Println("hashset contains 10")
 	}
 
 	l.Range(func(value int64) bool {
-		fmt.Println("skipset range found ", value)
+		fmt.Println("hashset range found ", value)
 		return true
 	})
 
 	l.Remove(15)
-	fmt.Printf("skipset contains %d items\r\n", l.Len())
+	fmt.Printf("hashset contains %d items\r\n", l.Len())
 }
 
 func TestIntSet(t *testing.T) {
 	// Correctness.
 	l := NewInt64()
-	if l.length != 0 {
+	if l.Len() != 0 {
 		t.Fatal("invalid length")
 	}
 	if l.Contains(0) {
 		t.Fatal("invalid contains")
 	}
 
-	if !l.Add(0) || l.length != 1 {
+	if !l.Add(0) || l.Len() != 1 {
 		t.Fatal("invalid add")
 	}
 	if !l.Contains(0) {
 		t.Fatal("invalid contains")
 	}
-	if !l.Remove(0) || l.length != 0 {
+	if !l.Remove(0) || l.Len() != 0 {
 		t.Fatal("invalid remove")
 	}
 
-	if !l.Add(20) || l.length != 1 {
+	if !l.Add(20) || l.Len() != 1 {
 		t.Fatal("invalid add")
 	}
-	if !l.Add(22) || l.length != 2 {
+	if !l.Add(22) || l.Len() != 2 {
 		t.Fatal("invalid add")
 	}
-	if !l.Add(21) || l.length != 3 {
+	if !l.Add(21) || l.Len() != 3 {
 		t.Fatal("invalid add")
 	}
 	if l.Add(21) {
-		t.Fatal(l.length, " invalid add")
+		t.Fatal(l.Len(), " invalid add")
 	}
 }
